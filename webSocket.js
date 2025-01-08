901e88c262a27ccf35cc57f6cb3884388a4d346e
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-01-08 16:41:23 +0530

    ws update

--- webSocket.js
+++ webSocket.js
@@ -1,62 +1,68 @@
 const WebSocket = require('ws')
 const userModel = require('./models/user.model')
+const captainModel = require('./models/captain.model')
 
 
 let webSocketServer
 
 
-const initializeSocket = (server)=>{
-    webSocketServer = new WebSocket.Server({server})
+const initializeSocket = (server) => {
+    webSocketServer = new WebSocket.Server({ server })
 
-    webSocketServer.on('connection',(ws)=>{
+    webSocketServer.on('connection', (ws) => {
         console.log('Client Connected')
 
-        ws.on('message',async(msg)=>{
-            try{
+        ws.on('message', async (msg) => {
+            try {
                 const data = JSON.parse(msg)
-                if(data.event =='join'){
-                    const{userId,userType} = data
+                if (data.event == 'join') {
+                    const { userId, userType } = data
 
-                    if(userType == 'user'){
-                        await userModel.findByIdAndUpdate(userId,{socketId:ws.id})
+                    if (userType == 'user') {
+                        await userModel.findByIdAndUpdate(userId, { socketId: ws.id })
                     }
-                    else if(userType=='captain'){
-                        //Do something
+                    else if (userType == 'captain') {
+                        await captainModel.findByIdAndUpdate(userId, { socketId: ws.id })
                     }
                 }
-                else if(data.event == 'update-location-captain'){
-                    const {userId,location} = data
+                else if (data.event == 'update-location-captain') {
+                    const { userId, location } = data
 
-                    if(!location || !location.latitude || !location.longitude){
-                        return ws.send(JSON.stringify({event:'error',message:'Invalid Location data'}))
+                    if (!location || !location.latitude || !location.longitude) {
+                        return ws.send(JSON.stringify({ event: 'error', message: 'Invalid Location data' }))
                     }
-                    //Do something with captain Model
+                    await captainModel.findByIdAndUpdate(userId, {
+                        location: {
+                            latitude: location.latitude,
+                            longitude: location.longitude
+                        }
+                    })
                 }
-            }catch(error){
+            } catch (error) {
                 console.error('Error handling message:', error);
                 ws.send(JSON.stringify({ event: 'error', message: 'Invalid message format' }));
-            
+
             }
         })
 
-        ws.on('close',()=>{
+        ws.on('close', () => {
             console.log('Client disconnected')
         })
     })
 }
 
 
-const sendMessageToSocketId = (socketId,messageObject)=>{
+const sendMessageToSocketId = (socketId, messageObject) => {
     console.log(messageObject)
-    if(webSocketServer){
-        webSocketServer.clients.forEach((client)=>{
-            if(client.readyState === WebSocket.OPEN && client.id ===socketId){
+    if (webSocketServer) {
+        webSocketServer.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN && client.id === socketId) {
                 client.send(JSON.stringify(messageObject))
             }
         })
-    }else{
+    } else {
         console.log("websocket server not initialized")
     }
 }
 
-module.exports ={initializeSocket,sendMessageToSocketId}
+module.exports = { initializeSocket, sendMessageToSocketId }
